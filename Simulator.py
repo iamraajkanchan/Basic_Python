eafe2c2cbc460dc7033d61b082839b93e856d16f
iamraajkanchan <iamraajkanchan@gmail.com>
2025-07-12 09:55:06 +0530

    Example regarding a class, object and methods are added in the project

---
+++ Simulator.py
@@ -0,0 +1,7 @@
+# To run the program, use the command `python Simulator.py`
+from basics.Basic import Basic
+# basics is the name of the package and Basic.py is the name of file which contains the Basic class
+
+basic = Basic()
+basic.simpleMethod()
+basic.changeProgrammerName("Abhishek Tiwari")
---
+++ basics/Basic.py
@@ -0,0 +1,23 @@
+class Basic:    
+
+    programmerName = "Raj Kanchan"
+
+    def __init__(self):
+        print("Basic class initialized.")
+
+    '''
+    # self is just a convention - you can name it anything you want, but everyone uses 'self'of.
+    # It's automatically passed by Python when you call the method.
+    # self represents the specific instance of the class, allowing access to its attributes and methods.
+    # Without self, you cannot access instance variables or methods from within the class.
+
+    '''
+
+    def simpleMethod(self):
+        print("This is a simple method in the Basic class.")
+
+    def changeProgrammerName (self, newName):
+
+        print(f"Changing programmer name from {self.programmerName} to {newName}")
+        self.programmerName = newName
+        print(f"Programmer name changed to {self.programmerName}")
