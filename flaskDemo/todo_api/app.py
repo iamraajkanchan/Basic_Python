bb9b5576a17b60a092e65dfd4100f77c3871bdd9
iamraajkanchan <iamraajkanchan@gmail.com>
2025-08-24 05:08:16 +0530

    Fixed time difference issue.

--- flaskDemo/todo_api/app.py
+++ flaskDemo/todo_api/app.py
@@ -34,7 +34,6 @@ def index():
 def ping():
     return "pong"
 
-
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
--- flaskDemo/todo_api/models.py
+++ flaskDemo/todo_api/models.py
@@ -1,11 +1,13 @@
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import pytz
 
 db = SQLAlchemy()
+ist = pytz.timezone("Asia/Kolkata")
 
 class ToDo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, nullable=False)
     title = db.Column(db.String(200), nullable=False)
     description = db.Column(db.String(255))
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    created_at = db.Column(db.DateTime, default=lambda: datetime.now(ist) )
